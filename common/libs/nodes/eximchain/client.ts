5f8f3a2e9a859f1b9e16712c9c2b214fbc815a80
Wang Zuo <wzuoadjusted@gmail.com>
2018-07-26 17:05:24 +0800

    fix json rpc bug from go-kit

--- common/libs/nodes/eximchain/client.ts
+++ common/libs/nodes/eximchain/client.ts
@@ -1,5 +1,5 @@
 import RPCClient from '../rpc/client';
-import { RPCRequest } from '../rpc/types';
+import { JsonRpcResponse, RPCRequest } from '../rpc/types';
 
 export default class EximchainClient extends RPCClient {
   public decorateRequest = (req: RPCRequest) => ({
@@ -8,4 +8,28 @@ export default class EximchainClient extends RPCClient {
     jsonrpc: '2.0',
     params: req.params || []
   });
+
+  public call = (request: RPCRequest | any): Promise<JsonRpcResponse> => {
+    return fetch(this.endpoint, {
+      method: 'POST',
+      headers: this.createHeaders({
+        'Content-Type': 'application/json',
+        ...this.headers
+      }),
+      body: JSON.stringify(this.decorateRequest(request))
+    })
+      .then(r => r.json())
+      .then(json => {
+        if (!json.error) delete json.error;
+        return json;
+      });
+  };
+
+  private createHeaders = (headerObject: HeadersInit) => {
+    const headers = new Headers();
+    Object.entries(headerObject).forEach(([name, value]) => {
+      headers.append(name, value);
+    });
+    return headers;
+  };
 }
